1ee07cf7549ef607132d3612d580708c5a9b0479
Felipe Panosso <felipepanosso21@gmail.com>
2025-07-02 08:56:12 -0300

    feat(patient): implement PatientRepository

---
+++ patient/src/main/java/com/service/patient/repository/PatientRepository.java
@@ -0,0 +1,11 @@
+package com.service.patient.repository;
+
+import com.service.patient.model.PatientModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface PatientRepository extends JpaRepository<PatientModel, UUID> {
+}
