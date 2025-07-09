a4683632ee8523369eae67c5293f9152937394e0
Felipe Panosso <felipepanosso21@gmail.com>
2025-07-09 14:38:16 -0300

    feat(PatientRepository): added a new method in PatientRepository

--- patient/src/main/java/com/service/patient/repository/PatientRepository.java
+++ patient/src/main/java/com/service/patient/repository/PatientRepository.java
@@ -9,4 +9,5 @@ import java.util.UUID;
 @Repository
 public interface PatientRepository extends JpaRepository<PatientModel, UUID> {
     boolean existsByEmail(String email);
+    boolean existsByEmailAndIdNot(String email, UUID id);
 }
